63a0567b8ff01f5d39378e9fb21b68c990cec225
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2025-04-01 15:08:16 +0200

    Elements Formulaire

--- app/routes/Elements/ElementFormulaire.jsx
+++ app/routes/Elements/ElementFormulaire.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 // import styled from "styled-components";
 
-export default function Input({ type, id, nom }) {
+
+// Pour tous les inputs en texte (texte, mail, tel, url...)
+export default function InputTexte({ type, id, nom }) {
     return (
         <div className="input">
             <label htmlFor={id}>{nom}</label>
-            <input type={type} name={id} id={id}/>
+            <input type={type} name={id} id={id}></input>
+        </div>
+    );
+}
+
+// Pour un textarea
+export function TextArea({ id, nom }) {
+    return (
+        <div className="input">
+            <label htmlFor={id}>{nom}</label>
+            <textarea name={id} id={id}></textarea>
+        </div>
+    );
+}
+
+//Pour une radio box. 
+export function InputRadio({ name, id, nom}){
+    return (
+        <div>
+            <input type="radio" name={name} id={id} value={id}/>
+            <label htmlFor={id}>{nom}</label>
+        </div>
+    );
+}
+
+// Pour une CheckBox
+export function InputCheckBox({id, nom}){
+    return (
+        <div>
+            <input type="checkbox" name={id} id={id} />
+            <label htmlFor={id}>{nom}</label>
         </div>
     );
 }
